64cabb92d79ad2051fabf380f9858dac4de6ab1b
Robert Nowotny <bitranox@users.noreply.github.com>
2017-11-30 18:42:24 +0100

    Update test_wrapt_timeout_decorator.py

--- tests/test_wrapt_timeout_decorator.py
+++ tests/test_wrapt_timeout_decorator.py
@@ -80,7 +80,7 @@ def test_function_name(use_signals):
 
 
 def test_timeout_pickle_error():
-    """Test that when a pickle error occurs a timeout error is raised."""
+    """Test that when a pickle error occurs a timeout error is raised"""
     # codecov start ignore
     @timeout(dec_timeout=1, use_signals=False)
     def f():
