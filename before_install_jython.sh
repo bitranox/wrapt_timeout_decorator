1aec6904296c96bfabd7ae1b24a7ad3f085bbcb5
Robert Nowotny <bitranox@users.noreply.github.com>
2019-02-25 14:35:05 +0100

    dropped python 2.6 support, since wrapt dropped it

--- before_install_jython.sh
+++ before_install_jython.sh
@@ -15,16 +15,22 @@
 
 set -e
 
-pip install jip==0.7
+# pip install jip==0.7
+echo "step1"
+pip install jip
+echo "step2"
 jip install $JYTHON
+echo "step3"
 NON_GROUP_ID=${JYTHON#*:}
 _JYTHON_BASENAME=${NON_GROUP_ID/:/-}
 OLD_VIRTUAL_ENV=$VIRTUAL_ENV
+echo "step4"
 java -jar $OLD_VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $HOME/jython
 
 BEFORE_PY_26=$($HOME/jython/bin/jython -c "import sys; print sys.version_info < (2, 6)")
 if [ "$BEFORE_PY_26" == "True" ]
 then
+    echo "step5"
     # Travis CI virtualenv version is greater 1.9.1, which was the
     # last version compatible with Python < 2.6
     pip install virtualenv==1.9.1
@@ -36,6 +42,7 @@ virtualenv --distribute -p $HOME/jython/bin/jython $HOME/myvirtualenv
 
 if [ "$BEFORE_PY_26" == "True" ]
 then
+    echo "step6"
     # No SSL support for Jython
     cat > $HOME/myvirtualenv/pip.conf <<EOF
 [install]
