8470ce5bcb8b23780bac11eea602919274a27364
Robert Nowotny <bitranox@users.noreply.github.com>
2019-02-26 10:24:07 +0100

    refractoring test

--- wrapt_timeout_decorator/wrapt_timeout_decorator.py
+++ wrapt_timeout_decorator/wrapt_timeout_decorator.py
@@ -25,154 +25,12 @@ if sys.version_info < (3, 3):
     TimeoutError = AssertionError  # there is no TimeoutError below Python 3.3
 
 
-def _raise_exception(exception, exception_message):
-    """ This function checks if a exception message is given.
-    If there is no exception message, the default behaviour is maintained.
-    If there is an exception message, the message is passed to the exception.
-    """
-    if not exception:
-        exception = TimeoutError
-    raise exception(exception_message)
-
-
-def _get_bad_pickling_types(object_to_pickle):
-    bad_types = list()
-    try:
-        bad_types = dill.detect.badtypes(object_to_pickle)
-    except NotImplementedError:
-        pass
-    finally:
-        return bad_types
-
-
-def detect_unpickable_objects_and_reraise(object_to_pickle):
-    bad_types = _get_bad_pickling_types(object_to_pickle)
-    if hasattr(object_to_pickle, '__name__'):
-        object_name = object_to_pickle.__name__
-    else:
-        object_name = 'object'
-    s_err = 'can not pickle {on}, bad types {bt}'.format(on=object_name, bt=bad_types)
-    raise dill.PicklingError(s_err)
-
-
-def timeout2(dec_timeout=None, use_signals=True, timeout_exception=None, exception_message=None, dec_allow_eval=False):
-    @wrapt.decorator
-    def wrapper(wrapped, instance, args, kwargs):
-        wrap_helper = WrapHelper(dec_timeout, use_signals, timeout_exception, exception_message, dec_allow_eval)
-        wrap_helper.set_signals_to_false_if_not_possible()
-        wrap_helper.get_kwargs(kwargs)
-        if wrap_helper.should_eval:
-            wrap_helper.dec_timeout = eval(str(wrap_helper.dec_timeout))
-        wrap_helper.format_exception_message(wrapped)
-        if not wrap_helper.dec_timeout:
-            return wrapped(*args, **kwargs)
-        else:
-            if wrap_helper.use_signals:
-                try:
-                    wrap_helper.save_old_and_set_new_alarm_handler()
-                    return wrapped(*args, **kwargs)
-                finally:
-                    wrap_helper.restore_old_alarm_handler()
-            else:
-                try:
-                    timeout_wrapper = _Timeout(wrapped, wrap_helper.timeout_exception,
-                                               wrap_helper.exception_message, wrap_helper.dec_timeout)
-                    return timeout_wrapper(*args, **kwargs)
-                except dill.PicklingError:
-                    wrap_helper.detect_unpickable_objects_and_reraise(wrapped)
-    return wrapper
-
-
-class WrapHelper(object):
-    def __init__(self, dec_timeout=None, use_signals=True, timeout_exception=None,
-                 exception_message=None, dec_allow_eval=False):
-        self.dec_timeout = dec_timeout
-        self.use_signals = use_signals
-        self.timeout_exception = timeout_exception
-        self.exception_message = exception_message
-        self.dec_allow_eval = dec_allow_eval
-        self.old_alarm_handler = None
-
-    def get_kwargs(self, kwargs):
-        self.dec_allow_eval = kwargs.pop('dec_allow_eval', self.dec_allow_eval)  # make mutable and get possibly kwarg
-        self.dec_timeout = kwargs.pop('dec_timeout', self.dec_timeout)   # make mutable and get possibly kwarg
-
-    def should_eval(self):
-        if self.dec_allow_eval and isinstance(self.dec_timeout, str):
-            return True
-        else:
-            return False
-
-    def format_exception_message(self, wrapped):
-        if hasattr(wrapped, '__name__'):
-            function_name = wrapped.__name__
-        else:
-            function_name = '(unknown name)'
-        if not self.exception_message:
-            self.exception_message = 'Function {f} timed out after {s} seconds'.format(f=function_name, s=self.dec_timeout)
-
-    def new_alarm_handler(self, signum, frame):
-        self.raise_exception()
-
-    def save_old_and_set_new_alarm_handler(self):
-        self.old_alarm_handler = signal.signal(signal.SIGALRM, self.new_alarm_handler)
-        signal.setitimer(signal.ITIMER_REAL, self.dec_timeout)
-
-    def restore_old_alarm_handler(self):
-        signal.setitimer(signal.ITIMER_REAL, 0)
-        signal.signal(signal.SIGALRM, self.old_alarm_handler)
-
-    def set_signals_to_false_if_not_possible(self):
-        """ gives True when we can use timeout signals, otherwise False"""
-        if platform.system().lower().startswith('win'):  # on Windows we cant use Signals
-            self.use_signals = False
-        if sys.version_info < (3, 4):
-            # on old python use this method - we can only use Signals in the Main Thread
-            if not isinstance(threading.current_thread(), threading._MainThread):
-                self.use_signals = False
-        else:
-            # much nicer after python 3.4 - we can only use Signals in the Main Thread
-            if not threading.current_thread() == threading.main_thread():
-                self.use_signals = False
-
-    def detect_unpickable_objects_and_reraise(self, object_to_pickle):
-        # sometimes the detection detects unpickable objects but actually
-        # they can be pickled - so we just try to start the thread and report
-        # the unpickable objects if that fails
-        bad_types = self.get_bad_pickling_types(object_to_pickle)
-        if hasattr(object_to_pickle, '__name__'):
-            object_name = object_to_pickle.__name__
-        else:
-            object_name = 'object'
-        s_err = 'can not pickle {on}, bad types {bt}'.format(on=object_name, bt=bad_types)
-        raise dill.PicklingError(s_err)
-
-    @staticmethod
-    def get_bad_pickling_types(object_to_pickle):
-        bad_types = list()
-        try:
-            bad_types = dill.detect.badtypes(object_to_pickle)
-        except NotImplementedError:
-            pass
-        finally:
-            return bad_types
-
-    def raise_exception(self):
-        """ This function checks if a exception message is given.
-        If there is no exception message, the default behaviour is maintained.
-        If there is an exception message, the message is passed to the exception.
-        """
-        if not self.timeout_exception:
-            self.timeout_exception = TimeoutError
-        raise self.timeout_exception(self.exception_message)
-
-
 def timeout(dec_timeout=None, use_signals=True, timeout_exception=None, exception_message=None, dec_allow_eval=False):
     """Add a timeout parameter to a function and return it.
 
-    ToDo : Traceback information when using no_signals
-           integrate tblib in order to get exceptions when use_signals=False
-           (see https://pypi.python.org/pypi/tblib)
+    ToDo :   Traceback information when using no_signals
+                integrate tblib in order to get complete Traceback when use_signals=False
+                (see https://pypi.python.org/pypi/tblib)
 
     Usage:
 
@@ -247,79 +105,113 @@ def timeout(dec_timeout=None, use_signals=True, timeout_exception=None, exceptio
 
     @wrapt.decorator
     def wrapper(wrapped, instance, args, kwargs):
-        exc_msg = exception_message                             # make mutable
-        decm_allow_eval = kwargs.pop('dec_allow_eval', dec_allow_eval)  # make mutable and get possibly kwarg
-        decm_timeout = kwargs.pop('dec_timeout', dec_timeout)   # make mutable and get possibly kwarg
-        if decm_allow_eval and isinstance(dec_timeout, str):
-            decm_timeout = eval(decm_timeout)                   # if allowed evaluate timeout
-        if not exc_msg:
-            exc_msg = 'Function {f} timed out after {s} seconds'.format(f=wrapped.__name__, s=decm_timeout)
-        if not decm_timeout:
+        wrap_helper = WrapHelper(dec_timeout, use_signals, timeout_exception, exception_message, dec_allow_eval)
+        wrap_helper.set_signals_to_false_if_not_possible()
+        wrap_helper.get_kwargs(kwargs)
+        if wrap_helper.should_eval:
+            wrap_helper.dec_timeout = eval(str(wrap_helper.dec_timeout))
+        wrap_helper.format_exception_message(wrapped)
+        if not wrap_helper.dec_timeout:
             return wrapped(*args, **kwargs)
         else:
-            if b_signals:
-                old_alarm_handler = _return_old_and_set_new_alarm_handler(timeout_exception, exc_msg, decm_timeout)
+            if wrap_helper.use_signals:
                 try:
+                    wrap_helper.save_old_and_set_new_alarm_handler()
                     return wrapped(*args, **kwargs)
                 finally:
-                    _reset_and_restore_old_alarm_handler(old_alarm_handler)
+                    wrap_helper.restore_old_alarm_handler()
             else:
                 try:
-                    timeout_wrapper = _Timeout(wrapped, timeout_exception, exc_msg, decm_timeout)
+                    timeout_wrapper = _Timeout(wrapped, wrap_helper.timeout_exception,
+                                               wrap_helper.exception_message, wrap_helper.dec_timeout)
                     return timeout_wrapper(*args, **kwargs)
                 except dill.PicklingError:
-                    # sometimes the detection detects unpickable objects but actually
-                    # they can be pickled - so we just try to start the thread and report
-                    # the unpickable objects if that fails
-                    detect_unpickable_objects_and_reraise(wrapped)
-
-    # automatically disable signals when they cant be used
-    if can_use_timeout_signals():
-        b_signals = use_signals
-    else:
-        b_signals = False
-
+                    wrap_helper.detect_unpickable_objects_and_reraise(wrapped)
     return wrapper
 
 
-def can_use_timeout_signals():
-    """ gives True when we can use timeout signals, otherwise False"""
-    if platform.system().lower().startswith('win'):  # on Windows we cant use Signals
-        return False
-    if sys.version_info < (3, 4):
-        # on old python use this method - we can only use Signals in the Main Thread
-        return isinstance(threading.current_thread(), threading._MainThread)
-    else:
-        # much nicer after python 3.4 - we can only use Signals in the Main Thread
-        return threading.current_thread() == threading.main_thread()
+class WrapHelper(object):
+    def __init__(self, dec_timeout=None, use_signals=True, timeout_exception=None,
+                 exception_message=None, dec_allow_eval=False):
+        self.dec_timeout = dec_timeout
+        self.use_signals = use_signals
+        self.timeout_exception = timeout_exception
+        self.exception_message = exception_message
+        self.dec_allow_eval = dec_allow_eval
+        self.old_alarm_handler = None
 
+    def get_kwargs(self, kwargs):
+        self.dec_allow_eval = kwargs.pop('dec_allow_eval', self.dec_allow_eval)  # make mutable and get possibly kwarg
+        self.dec_timeout = kwargs.pop('dec_timeout', self.dec_timeout)   # make mutable and get possibly kwarg
 
-def _target(child_conn, function, *args, **kwargs):
-    """Run a function with arguments and return output via a queue.
-    This is a helper function for the Process created in _Timeout. It runs
-    the function with positional arguments and keyword arguments and then
-    returns the function's output by way of a queue. If an exception gets
-    raised, it is returned to _Timeout to be raised by the value property.
-    """
-    try:
-        child_conn.send((True, function(*args, **kwargs)))
-    except:
-        child_conn.send((False, sys.exc_info()[1]))
-    finally:
-        child_conn.close()
+    def should_eval(self):
+        if self.dec_allow_eval and isinstance(self.dec_timeout, str):
+            return True
+        else:
+            return False
 
+    def format_exception_message(self, wrapped):
+        if hasattr(wrapped, '__name__'):
+            function_name = wrapped.__name__
+        else:
+            function_name = '(unknown name)'
+        if not self.exception_message:
+            self.exception_message = 'Function {f} timed out after {s} seconds'.format(f=function_name, s=self.dec_timeout)
 
-def _return_old_and_set_new_alarm_handler(timeout_exception, exc_msg, decm_timeout):
-    def new_alarm_handler(signum, frame):
-        _raise_exception(timeout_exception, exc_msg)
-    old_alarm_handler = signal.signal(signal.SIGALRM, new_alarm_handler)
-    signal.setitimer(signal.ITIMER_REAL, decm_timeout)
-    return old_alarm_handler
+    def new_alarm_handler(self, signum, frame):
+        _raise_exception(self.timeout_exception, self.exception_message)
 
+    def save_old_and_set_new_alarm_handler(self):
+        self.old_alarm_handler = signal.signal(signal.SIGALRM, self.new_alarm_handler)
+        signal.setitimer(signal.ITIMER_REAL, self.dec_timeout)
 
-def _reset_and_restore_old_alarm_handler(old_alarm_handler):
-    signal.setitimer(signal.ITIMER_REAL, 0)
-    signal.signal(signal.SIGALRM, old_alarm_handler)
+    def restore_old_alarm_handler(self):
+        signal.setitimer(signal.ITIMER_REAL, 0)
+        signal.signal(signal.SIGALRM, self.old_alarm_handler)
+
+    def set_signals_to_false_if_not_possible(self):
+        """ gives True when we can use timeout signals, otherwise False"""
+        if platform.system().lower().startswith('win'):  # on Windows we cant use Signals
+            self.use_signals = False
+        if sys.version_info < (3, 4):
+            # on old python use this method - we can only use Signals in the Main Thread
+            if not isinstance(threading.current_thread(), threading._MainThread):
+                self.use_signals = False
+        else:
+            # much nicer after python 3.4 - we can only use Signals in the Main Thread
+            if not threading.current_thread() == threading.main_thread():
+                self.use_signals = False
+
+    def detect_unpickable_objects_and_reraise(self, object_to_pickle):
+        # sometimes the detection detects unpickable objects but actually
+        # they can be pickled - so we just try to start the thread and report
+        # the unpickable objects if that fails
+        bad_types = self.get_bad_pickling_types(object_to_pickle)
+        if hasattr(object_to_pickle, '__name__'):
+            object_name = object_to_pickle.__name__
+        else:
+            object_name = 'object'
+        s_err = 'can not pickle {on}, bad types {bt}'.format(on=object_name, bt=bad_types)
+        raise dill.PicklingError(s_err)
+
+    @staticmethod
+    def get_bad_pickling_types(object_to_pickle):
+        bad_types = list()
+        try:
+            bad_types = dill.detect.badtypes(object_to_pickle)
+        except NotImplementedError:
+            pass
+        finally:
+            return bad_types
+
+    def raise_exception(self):
+        """ This function checks if a exception message is given.
+        If there is no exception message, the default behaviour is maintained.
+        If there is an exception message, the message is passed to the exception.
+        """
+        if not self.timeout_exception:
+            self.timeout_exception = TimeoutError
+        raise self.timeout_exception(self.exception_message)
 
 
 class _Timeout(object):
@@ -378,3 +270,28 @@ class _Timeout(object):
         if flag:
             return load
         raise load
+
+
+def _target(child_conn, function, *args, **kwargs):
+    """Run a function with arguments and return output via a queue.
+    This is a helper function for the Process created in _Timeout. It runs
+    the function with positional arguments and keyword arguments and then
+    returns the function's output by way of a queue. If an exception gets
+    raised, it is returned to _Timeout to be raised by the value property.
+    """
+    try:
+        child_conn.send((True, function(*args, **kwargs)))
+    except:
+        child_conn.send((False, sys.exc_info()[1]))
+    finally:
+        child_conn.close()
+
+
+def _raise_exception(exception, exception_message):
+    """ This function checks if a exception message is given.
+    If there is no exception message, the default behaviour is maintained.
+    If there is an exception message, the message is passed to the exception.
+    """
+    if not exception:
+        exception = TimeoutError
+    raise exception(exception_message)
