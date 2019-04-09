66d2e71c86b7b4dc53869ebfa2c9413cee97f9ea
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-09 22:45:26 +0200

    pypi initial commit

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ long_description = (
 
 setup(
     name='wrapt_timeout_decorator',
-    version='1.1.8',
+    version='1.1.9',
     description='Timeout decorator',
     long_description=long_description,
     author='Robert Nowotny',
