554ca45b71ad86cacc5166f782a8a4d8ab1bb8b0
bitranox <1uGToNSiYo85pWhQcqFu>
2019-05-17 00:26:38 +0200

    add typing

--- install_wine_machine.sh
+++ install_wine_machine.sh
@@ -14,8 +14,9 @@ check_wine_prefix
 check_wine_arch
 check_wine_windows_version
 check_headless_xvfb
+export_wine_version_number
 
-clr_green "Setup Wine Machine at ${WINEPREFIX}, WINEARCH=${WINEARCH}, wine_windows_version=${wine_windows_version}"
+clr_bold clr_green "Setup Wine Machine at ${WINEPREFIX}, WINEARCH=${WINEARCH}, wine_windows_version=${wine_windows_version}"
 mkdir -p ${WINEPREFIX}
 wine_drive_c_dir=${WINEPREFIX}/drive_c
 # xvfb-run --auto-servernum winecfg # fails marshal_object couldnt get IPSFactory buffer for interface ...
@@ -24,18 +25,24 @@ if [[ ${xvfb_framebuffer_service_active} == "True" ]]; then sudo service xvfb st
 winecfg
 if [[ ${xvfb_framebuffer_service_active} == "True" ]]; then sudo service xvfb start ; fi     # winecfg fails if xvfb server is running
 
-echo "Disable GUI Crash Dialogs"
+clr_bold clr_green "Disable GUI Crash Dialogs"
 winetricks nocrashdialog
 
-echo "Set Windows Version to ${wine_windows_version}"
+clr_bold clr_green "Set Windows Version to ${wine_windows_version}"
 winetricks -q ${wine_windows_version}
 
-echo "Install common Packets"
-
+clr_bold clr_green "Install common Packets :"
+clr_bold clr_green "install windowscodecs"
 retry winetricks -q windowscodecs
-retry winetricks -q msxml3
 
-clr_green "done"
-clr_green "******************************************************************************************************************"
+clr_bold clr_green "******************************************************************************************************************"
+clr_bold clr_green "install msxml3"
+if [[ ${wine_version_number} == "wine-4.8" ]]; then clr_bold clr_red "known regression, msxml3 does not work on wine-4.8" ; else retry winetricks -q msxml3 ; fi
+clr_bold clr_green "******************************************************************************************************************"
+clr_bold clr_green "install msxml6"
+retry winetricks -q msxml6
+clr_bold clr_green "******************************************************************************************************************"
+clr_bold clr_green "done"
+clr_bold clr_green "******************************************************************************************************************"
 clr_bold clr_green "FINISHED installing Wine Machine ${WINEPREFIX}"
-clr_green "******************************************************************************************************************"
+clr_bold clr_green "******************************************************************************************************************"
