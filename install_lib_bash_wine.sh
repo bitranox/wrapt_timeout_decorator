aba0cb9f2523bd81fef41f0ab28f95d74a03828d
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-06 21:22:07 +0200

    initial commit

--- install_lib_bash_wine.sh
+++ install_lib_bash_wine.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-function install_lib_bash_if_not_exist {
+function install_lib_bash_wine_if_not_exist {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
-    if [[ ! -d "${my_dir}/lib_bash" ]]; then
-        git clone https://github.com/bitranox/lib_bash.git ${my_dir}/lib_bash > /dev/null 2>&1
-        chmod -R +x ${my_dir}/lib_bash/*.sh
+    if [[ ! -d "${my_dir}/lib_bash_wine" ]]; then
+        git clone https://github.com/bitranox/lib_bash_wine.git ${my_dir}/lib_bash_wine > /dev/null 2>&1
+        chmod -R +x ${my_dir}/lib_bash_wine/*.sh
     fi
 }
 
-function update_lib_bash_if_exist {
+function update_lib_bash_wine_if_exist {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
-    if [[ -d "${my_dir}/lib_bash" ]]; then
-        cd lib_bash
+    if [[ -d "${my_dir}/lib_bash_wine" ]]; then
+        cd lib_bash_wine
         local git_remote_hash=$(git --no-pager ls-remote --quiet | grep HEAD | awk '{print $1;}' )
         local git_local_hash=$(git --no-pager log --decorate=short --pretty=oneline -n1 | grep HEAD | awk '{print $1;}' )
         git fetch --all  > /dev/null 2>&1
@@ -21,5 +21,5 @@ function update_lib_bash_if_exist {
     fi
 }
 
-update_lib_bash_if_exist
-install_lib_bash_if_not_exist
+update_lib_bash_wine_if_exist
+install_lib_bash_wine_if_not_exist
